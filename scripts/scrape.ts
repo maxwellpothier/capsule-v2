8ccd0ec35b3ac8b40018b8293a4d1d26e1e76737
maxwellpothier <pothiermaxwell@gmail.com>
2024-08-13 08:23:31 -0600

    finish scraping and save raw json

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -1,9 +1,11 @@
-import {Article, Headline} from "@/types";
+import {Article, Headline, ArticleChunk, CNBCJSON} from "@/types";
 import axios from "axios";
 import * as cheerio from "cheerio";
 import {encode} from "gpt-3-encoder";
+import * as fs from "fs";
 
 const BASE_URL = "https://www.cnbc.com";
+const CHUNK_SIZE = 200;
 
 const HEADLINE_CLASSNAMES = {
 	featured: ".FeaturedCard-packagedCardTitle",
@@ -12,7 +14,7 @@ const HEADLINE_CLASSNAMES = {
 	body: ".RiverHeadline-headline",
 };
 
-const getHeadlines = async () => {
+const getHeadlines = async (): Promise<Headline[]> => {
 	const html = await axios.get(BASE_URL);
 	const $ = cheerio.load(html.data);
 
@@ -44,12 +46,14 @@ const getHeadlines = async () => {
 		.flat();
 };
 
-const getArticle = async (headline: Headline) => {
+const getArticle = async (headline: Headline): Promise<Article> => {
 	if (!headline.url.startsWith(BASE_URL)) {
 		return {
 			headline,
 			keyPoints: [],
-			articleContent: "",
+			content: "",
+			tokens: 0,
+			chunks: [],
 		};
 	}
 
@@ -85,7 +89,7 @@ const getArticle = async (headline: Headline) => {
 	const article: Article = {
 		headline,
 		keyPoints: getKeyPoints(),
-		articleContent,
+		content: articleContent,
 		tokens: encode(articleContent).length,
 		chunks: [],
 	};
@@ -93,10 +97,102 @@ const getArticle = async (headline: Headline) => {
 	return article;
 };
 
+const getArticleChunks = async (article: Article): Promise<Article> => {
+	const {headline, keyPoints, content, tokens, chunks} = article;
+
+	const articleTextChunks: string[] = [];
+
+	if (encode(content).length <= CHUNK_SIZE) {
+		articleTextChunks.push(content.trim());
+	} else {
+		const split = content.split(". ");
+		let currentChunk = "";
+
+		for (let i = 0; i < split.length; i++) {
+			const sentence = split[i];
+			const sentenceTokens = encode(sentence).length;
+
+			if (currentChunk.length + sentenceTokens > CHUNK_SIZE) {
+				articleTextChunks.push(currentChunk.trim());
+				currentChunk = "";
+			}
+
+			if (sentence[sentence.length - 1].match(/[a-z0-9]/i)) {
+				currentChunk += sentence + ". ";
+			} else {
+				currentChunk += sentence + " ";
+			}
+		}
+
+		articleTextChunks.push(currentChunk.trim());
+	}
+
+	const articleChunks: ArticleChunk[] = articleTextChunks.map(chunk => {
+		return {
+			article_title: headline.title,
+			article_url: headline.url,
+			content: chunk,
+			content_tokens: encode(chunk).length,
+			embedding: [],
+		};
+	});
+
+	const keyPointsChunk = keyPoints.join(" ");
+	const keyPointsChunkTokens = encode(keyPointsChunk).length;
+
+	if (keyPointsChunkTokens < CHUNK_SIZE) {
+		articleChunks.push({
+			article_title: headline.title,
+			article_url: headline.url,
+			content: keyPointsChunk,
+			content_tokens: keyPointsChunkTokens,
+			embedding: [],
+		});
+	}
+
+	if (articleChunks.length > 1) {
+		for (let i = 0; i < articleChunks.length; i++) {
+			const chunk = articleChunks[i];
+			const prevChunk = articleChunks[i - 1];
+
+			if (chunk.content_tokens < 100 && prevChunk) {
+				prevChunk.content += chunk.content;
+				prevChunk.content_tokens = encode(prevChunk.content).length;
+				articleChunks.splice(i, 1);
+			}
+		}
+	}
+
+	return {
+		...article,
+		chunks: articleChunks,
+	};
+};
+
 (async () => {
 	const headlines = await getHeadlines();
-	const articles = await Promise.all(
-		headlines.map(async headline => await getArticle(headline))
-	);
-	console.log(articles);
+	const articles = (
+		await Promise.all(
+			headlines.map(async headline => await getArticle(headline))
+		)
+	).filter(article => article.content !== "");
+
+	const fullArticles: Article[] = [];
+
+	await articles.forEach(async article => {
+		const chunkedArticle = await getArticleChunks(article);
+		fullArticles.push(chunkedArticle);
+	});
+
+	const json: CNBCJSON = {
+		tokens: articles.reduce((acc, article) => acc + article.tokens, 0),
+		articles: fullArticles,
+	};
+
+	const filePath = "scripts/cnbc.json";
+
+	fs.writeFileSync(filePath, "");
+	fs.writeFileSync(filePath, JSON.stringify(json, null, 2));
+
+	console.log("Data has been written to scripts/cnbc.json");
 })();
--- types/index.ts
+++ types/index.ts
@@ -6,15 +6,20 @@ export type Headline = {
 export type Article = {
 	headline: Headline;
 	keyPoints: string[];
-	articleContent: string;
+	content: string;
 	tokens: number;
 	chunks: ArticleChunk[];
 };
 
 export type ArticleChunk = {
-	essay_title: string;
-	essay_url: string;
+	article_title: string;
+	article_url: string;
 	content: string;
 	content_tokens: number;
 	embedding: number[];
+};
+
+export type CNBCJSON = {
+	tokens: number;
+	articles: Article[];
 };
